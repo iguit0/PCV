e97748916fc9195e9cddbca1d89b219e134e800e
Igor Alves <igorsk89@gmail.com>
2019-12-05 15:52:15 -0300

    Versão Final

--- PCV.h
+++ PCV.h
@@ -7,9 +7,9 @@
 
 using namespace std;
 
-class PCV
-{
+class PCV {
 public:
+
 	//objeto para leitura de arquivo
 	ifstream f;
 
@@ -24,32 +24,55 @@ public:
 	int N;
 
 	//Matriz de distancias
-	vector<vector<double>> matriz_dist;
+	vector< vector<double> > matriz_dist;
 
 	//Imprimir solucao
 	void imprimeSol(vector<int> &sol);
-
+	
 	//Funcao de avaliacao
 	double fo(vector<int> &sol);
-
+	
 	//Gera solucao aleatoria
 	vector<int> geraSolAleatoria();
 
 	//Gera solucao pelo Vizinho Mais Proximo
 	vector<int> geraSolVizProx();
-
+	
 	//Retorna a posicao da cidade mais proxima na LC
-	int retornaMenorDist(int cidadeAtual, vector<int> &LC);
+	int retornaMenorDist(int cidadeAtual, vector<int>& LC);
 
 	//Gera solucao pela Insercao Mais Barata
 	vector<int> geraSolInsMaisBar();
-
+	
 	//Retorna a posicao da cidade com a insercao mais barata na LC
-	pair<int, int> retornaMenorIns(vector<int> &LC, vector<int> &sol);
+	pair<int,int> retornaMenorIns(vector<int>& LC, vector<int>& sol);
 
-	//Metodo de descida completa com movimento de troca
-	void descidaCompletaTroca(vector<int> &solCorrente, double &melhorFo);
+	//Metodo de descida completa com o movimento de troca
+	void descidaCompletaTroca(vector<int>& solCorrente, double& melhorFo);
+	
+	//Metodo de descida com primeiro de melhora com o movimento de troca
+	void descidaPrimeiraTroca(vector<int>& solCorrente, double& melhorFo);
 
-	//Metodo de descida primeiro de melhora com movimento de troca
-	void descidaPrimeiraTroca(vector<int> &solCorrente, double &melhorFo);
+	//Multi-Start
+	void multiStart(vector<int>& sol, double& foSol, int iterMax);
+
+	//Gera solucao pelo Vizinho Mais Proximo GRASP
+	vector<int> geraSolVizProxGRASP(double alpha);
+
+	//Retorna a posicao da cidade a partir do GRASP
+	int retornaMenorDistGRASP(int cidadeAtual, vector<int>& LC, double alpha);
+
+	//GRASP
+	void GRASP(vector<int>& melhorSol, double& melhorFo, int iterMax, double alpha);
+
+
+
+
+
+
+
+
+
+
+
 };
--- main.cpp
+++ main.cpp
@@ -12,59 +12,73 @@ int main()
 	//srand(time(NULL));
 	srand(1);
 
-	PCV p("A5.txt");
+	PCV p("A280.txt");	
+	
+	vector<int> solAleatoria, solAleatoriaAux;
 
-	vector<int> solAleatoria = p.geraSolAleatoria();
+	solAleatoria = p.geraSolAleatoria();
 
-	vector<int> solAleatoriaAux = solAleatoria;
-
-	//solAleatoria = p.geraSolAleatoria();
+	solAleatoriaAux = solAleatoria;
 
 	cout << "Solucao gerada Aleatoriamente: ";
+	
+	p.imprimeSol(solAleatoria);
 
 	double foSolAleatoria = p.fo(solAleatoria);
 
 	double foSolAleatoriaAux = foSolAleatoria;
 
+	cout << "Fo: " << foSolAleatoria << endl;
+
+
+	p.GRASP(solAleatoria, foSolAleatoria, 10, 0.2);
+
+
 	p.imprimeSol(solAleatoria);
 
-	cout << "Fo: " << p.fo(solAleatoria) << endl;
+	cout << "Fo: " << foSolAleatoria << endl;
 
-	vector<int> solVizProx;
+	/*vector<int> solVizProx;
 
 	solVizProx = p.geraSolVizProx();
 
 	cout << "Solucao gerada pelo Vizinho Mais Proximo: ";
-
+	
 	p.imprimeSol(solVizProx);
 
 	cout << "Fo: " << p.fo(solVizProx) << endl;
-
+	
 	vector<int> solInsMaisBar;
 
 	solInsMaisBar = p.geraSolInsMaisBar();
 
 	cout << "Solucao gerada pela Insercao Mais Barata: ";
-
+	
 	p.imprimeSol(solInsMaisBar);
 
 	cout << "Fo: " << p.fo(solInsMaisBar) << endl;
 
-	cout << "Busca Local com movimentos de troca aplicada a solução aleatoria: " << endl;
+	cout << "Busca local com movimento de troca aplicada a solucao aleatoria..." << endl;
 
 	p.descidaCompletaTroca(solAleatoria, foSolAleatoria);
 
 	p.imprimeSol(solAleatoria);
+	
+	cout << "Solucao apos Busca Local com movimento de troca: " << foSolAleatoria << endl;
 
-	cout << "Solucao apos busca local c/ movimento de troca: " << foSolAleatoria << endl;
 
-	cout << "Busca Local c/ primeira melhora c/ movimento de troca aplicada a solução aleatoria: " << endl;
+	cout << "Busca local com Primeira Melhora com movimento de troca aplicada a solucao aleatoria..." << endl;
 
-	p.descidaPrimeiraTroca(solAleatoria, foSolAleatoria);
+	p.descidaPrimeiraTroca(solAleatoriaAux, foSolAleatoriaAux);
 
-	p.imprimeSol(solAleatoria);
+	p.imprimeSol(solAleatoriaAux);
+	
+	cout << "Solucao apos Busca Local com Primeira Melhora com movimento de troca: " << foSolAleatoriaAux << endl;*/
 
-	cout << "Solucao apos busca local c/ primeira melhora c/ movimento de troca: " << foSolAleatoria << endl;
+
+
+
+
 
 	return 1;
 }
